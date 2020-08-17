aaf9c161df5b9099209ead33cf06a53b35428d62
irik77587 <48862812+irik77587@users.noreply.github.com>
2020-08-17 21:02:05 +0600

    Update librbc.py

--- librbc.py
+++ librbc.py
@@ -14,11 +14,9 @@ def binModifier(num, bytesize):# destination bytesize is 3 for octal 4 for hexa
     f = [''.join(num[1][bytesize*i:bytesize*(i+1)]) for i in range(int(y))]
     return [d,f]
 
-octa = {'0':'000','1':'001','2':'010','3':'011','4':'100','5':'101','6':'110','7':'111'}
-hexal = {'8':'1000','9':'1001','a':'1010','b':'1011','c':'1100','d':'1101','e':'1110','f':'1111'}
-hexa = {k:'0'+v for k,v in octa.items()}
-hexa.update(hexal)
-del hexal
+hexa = {'0':'0000','1':'0001','2':'0010','3':'0011','4':'0100','5':'0101','6':'0110','7':'0111','8':'1000','9':'1001','a':'1010','b':'1011','c':'1100','d':'1101','e':'1110','f':'1111'}
+octa = {k:v[1:] for k,v in hexa.items()}
+octa = dict(list(octal.items())[:8])
 invOcta = {v:k for k,v in octa.items()}
 invHexa = {v:k for k,v in hexa.items()}
 def splitConverter(num, sample):
